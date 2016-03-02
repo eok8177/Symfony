8062cbe89b4aefbb24fd1709b250087ea282fa9d
Evgen Koromyslov <eok8177@gmail.com>
2016-03-02 21:10:00 +0200

    load routes defined as annotations

--- src/AppBundle/Controller/HelloController.php
+++ src/AppBundle/Controller/HelloController.php
@@ -3,9 +3,14 @@ namespace AppBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
 
 class HelloController extends Controller
 {
+	/**
+	 * @Route("/hello/{name}", name="hello")
+	 */
     public function indexAction($name)
     {
         return new Response('<html><body>Hello '.$name.'!</body></html>');
