469ecebfd636ff7662bff50a457b7a02a0186025
Evgen Koromyslov <eok8177@gmail.com>
2016-03-02 21:54:43 +0200

    DataBase

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,6 +6,9 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
+use AppBundle\Entity\News;
+use Symfony\Component\HttpFoundation\Response;
+
 class DefaultController extends Controller
 {
     /**
@@ -17,5 +20,41 @@ class DefaultController extends Controller
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
         ]);
+    }
+
+    /**
+     * @Route("/create", name="create")
+     */
+    public function createAction()
+    {
+        $news = new News();
+        $news->setName('A Foo Bar');
+        $news->setDescription('Lorem ipsum dolor');
+
+        $em = $this->getDoctrine()->getManager();
+
+        $em->persist($news);
+        $em->flush();
+
+        return new Response('Created news id '.$news->getId());
+    }
+
+    /**
+     * @Route("/show/{id}", name="showNews")
+     */
+    public function showAction($id)
+    {
+        $news = $this->getDoctrine()
+            ->getRepository('AppBundle:News')
+            ->find($id);
+
+        if (!$news) {
+            throw $this->createNotFoundException(
+                'No news found for id '.$id
+            );
+        }
+
+        // ... do something, like pass the $news object into a template
+        return new Response('View news description '.$news->getDescription());
     }
 }
---
+++ src/AppBundle/Entity/News.php~
@@ -0,0 +1,90 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * News
+ *
+ * @ORM\Table(name="news")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\NewsRepository")
+ */
+class News
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    protected $name;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    protected $description;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return News
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return News
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+}
